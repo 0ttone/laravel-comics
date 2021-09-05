9f442e0008edc5bbf9a6826c92316ddcef8ee46c
MyUsername <emanuele.marini37@gmail.com>
2021-09-05 16:27:31 +0200

    typo a go-go

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,26 +1,26 @@
 <nav class="top-nav">
       <div class="nav-container container">
             <div class="img-container">
-                  <img src="../img/dc-logo.png" alt="dc-logo">
+                  <img src="../images/dc-logo.png" alt="dc-logo">
 
             </div>
-      </div>
-      <div class="menu-container">
-            <ul>
-                        <li><a href="">CHARACTERS</a></li>
-                        <li><a href="">COMICS</a></li>
-                        <li><a href="">MOVIES</a></li>
-                        <li><a href="">TV</a></li>
-                        <li><a href="">GAMES</a></li>
-                        <li><a href="">COLLECTIBLES</a></li>
-                        <li><a href="">VIDEO</a></li>
-                        <li ><a href="">FANS</a></li>
-                        <li><a href="">NEWS</a></li>
-                        <li><a href="">SHOP</a></li>
-                  
-            </ul>
-      </div>
+      
+            <div class="menu-container">
+                  <ul>
+                              <li><a href="#">CHARACTERS</a></li>
+                              <li><a href="#">COMICS</a></li>
+                              <li><a href="#">MOVIES</a></li>
+                              <li><a href="#">TV</a></li>
+                              <li><a href="#">GAMES</a></li>
+                              <li><a href="#">COLLECTIBLES</a></li>
+                              <li><a href="#">VIDEO</a></li>
+                              <li ><a href="#">FANS</a></li>
+                              <li><a href="#">NEWS</a></li>
+                              <li><a href="#">SHOP</a></li>
+                        
+                  </ul>
+            </div>
 
-
+      </div>
 
 </nav>
